823ac9faa3452e618d3266869d26c1456c1d66d6
Joseph <Jpatrick148@Gmail.com>
2018-11-06 09:53:50 -0500

    We out here

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Lab21.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.index = "Welcome To My Stuff n Junk. ";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Result(Registration r)
+        {
+            
+            return View(r);
+        }
+    }
+}
